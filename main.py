6c47b76c6b0e667660180fe9338570fd04c92922
payashi6907 <70702001+payashi6907@users.noreply.github.com>
2020-09-05 06:42:41 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import ar
 import time
 
-print
-
-
 initial_amount=10000
+initial_coin="USDT"
 
-arb = ar.Arbitrage("USDT", initial_amount*1000000000000000000)
+print("initial asset "+str(initial_amount)+" "+initial_coin)
+
+arb = ar.Arbitrage(initial_coin, initial_amount*1000000000000000000)
 
 while True:
     time.sleep(5)
     arb.check()
     arb.print()
-
 
