c7ff4cf32d819582a757efe4f222f56eaa48c274
payashi6907 <70702001+payashi6907@users.noreply.github.com>
2020-09-07 22:13:19 +0900

    Add files via upload

--- ar.py
+++ ar.py
@@ -34,11 +34,14 @@ class Arbitrage:
                 self.maxhistory_unit[cn]= max(uniswapv2_amount, curve_amount)/n18
                 self.current_rate[cn] = max(uniswapv2_rate, curve_rate)
         
-    def check(self, max_ratio=1.003, realmode=False):
+    def check(self, max_ratio=1.002, max_ratio_sUSD = 1.01, realmode=False):
         dex_list = {}
         compare_list = {}
         amount_list = {}
         ratio_list= {}
+        max_ratio_list={}
+        cheap_coin_num = 0
+        
 
         for cn in coinapi.coin_list:
             if cn != self.current_coin:
@@ -51,15 +54,28 @@ class Arbitrage:
                     dex_list[cn] = "Curve"
                 compare_list[cn] = self.current_rate[cn] * self.current_amount
                 amount_list[cn] = maxamount 
+            
+            if cn == "sUSD":
+                max_ratio_list[cn] = max_ratio_sUSD
+            else:
+                max_ratio_list[cn] = max_ratio
            
         max_cn = ""
 
         for cn in amount_list:
-            if amount_list[cn] > max_ratio * compare_list[cn]:
+            if amount_list[cn] > max(max_ratio , max_ratio_list[cn]) * compare_list[cn]:
                 max_cn = cn
                 max_ratio = amount_list[cn]/compare_list[cn]
                 # print(cn)
+            
             ratio_list[cn]=amount_list[cn]/compare_list[cn]
+            if ratio_list[cn] > 1.001:
+                cheap_coin_num += 1
+        
+        if cheap_coin_num >= 3:
+            print(str(self.current_coin)+" price incleased")
+        elif cheap_coin_num == 2 or cheap_coin_num == 1 :
+            print(str(max_cn)+" prise decreased")
 
         print("ValueRatio: "+ str(ratio_list))
 
--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Performing arbitrage trade of stable coin.') 
 parser.add_argument('--realmode', action='store_true')
-parser.add_argument('--ratio', type=float, default=1.003)
+parser.add_argument('--ratio', type=float, default=1.002)
+parser.add_argument('--ratio_sUSD', type=float, default=1.01)
 args = parser.parse_args()
 
-initial_amount=10000
+initial_amount=5000
 initial_coin="USDT"
 
 print("initial asset "+str(initial_amount)+" "+initial_coin)
@@ -17,7 +18,7 @@ itr = 0
 
 while True:
     time.sleep(5)
-    arb.check(max_ratio=args.ratio, realmode=args.realmode)
+    arb.check(max_ratio=args.ratio,max_ratio_sUSD = args.ratio_sUSD, realmode=args.realmode)
     ct = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
     print(str(ct) + ": itr = " + str(itr))
     # arb.print()
